28cee59956215f56a2f855d60ec003f001c8f901
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2019-06-15 20:44:56 +0200

    probando otra manera de mostrar los datos en el input

--- PHP POO PRUEBAS/CLASES/Search.php
+++ PHP POO PRUEBAS/CLASES/Search.php
@@ -11,20 +11,36 @@
    
     if ($usuarios!= null) {
         
-        foreach($usuarios as $usuario) {
-            $html .= '<div>
-            <a class="suggest-element" 
-            data="'.'" 
-            id="'.$usuario['id'].'"
-            >'.$usuario['nombre']." ".$usuario['apellidos'].'</a></div>';
+        //este array es para la primera form que si funciona
 
-            //$html .= "<p>".$usuario['nombre']."</p>";
-        }    
-        
-    }
+        // foreach($usuarios as $usuario) {
+        //     $html .= '<div>
+        //     <a class="suggest-element" 
+        //     data="'.'" 
+        //     id="'.$usuario['id'].'"
+        //     >'.$usuario['nombre']." ".$usuario['apellidos'].'</a></div>';
+
+        //     //$html .= "<p>".$usuario['nombre']."</p>";
+        // }    
+
+
+        // while($row = mysqli_fetch_array($result) ){
+        //    $response[] = array("value"=>$row['id'],"label"=>$row['name']);
+        //  }
+        foreach ($usuarios as $usuario) {
+           $response[] = array("value"=>$usuario['id'],"label"=>$usuario['nombre']." ".$usuario['apellidos']);  
+        }
+        echo json_encode($response);
+
+    }  
     else{
-        $html.="vacio!";
+       // $html.="vacio!";
+        echo "vacio!";
     }
 
-    echo $html;
+        //return de la primera forma que SI funciona
+    //echo $html;
+
+    exit;
+
 ?>
--- PHP POO PRUEBAS/VISTAS/buscador.php
+++ PHP POO PRUEBAS/VISTAS/buscador.php
@@ -28,39 +28,79 @@
 
 
 	<script>
-		$(document).ready(function() {
+	$(document).ready(function() {
+
+
+
+
 	    $('#key').on('keyup', function() {
 	        var key = $(this).val();		
 	        console.log(key);
 	        var dataString =key;
-		$.ajax({
-	            type: "POST",
-	            url: "../clases/Search.php",
-	            data: {key: dataString},
-	            success: function(data) {
-	            	if (data) {
-	            		 //Escribimos las sugerencias que nos manda la consulta
-			                $('#suggestions').fadeIn(1000).html(data);
-			                //Al hacer click en alguna de las sugerencias
-			                $('.suggest-element').on('click', function(){
-			                        //Obtenemos la id unica de la sugerencia pulsada
-			                        var id = $(this).attr('id');
-			                        //Editamos el valor del input con data de la sugerencia pulsada
-			                        $('#key').val($('#'+id).attr('data'));
-			                        //Hacemos desaparecer el resto de sugerencias
-			                        $('#suggestions').fadeOut(1000);
-			                        alert('Has seleccionado el '+id+' '+$('#'+id).attr('data'));
-			                        return false;
-			                });
-	            	}
-	            	else{
-	            		
-	            	}
-	               
-	            }
-	        });
-	    });
+
+					$( function() {
+
+					 // Single Select
+						$( "#key" ).autocomplete({
+							  source: function( request, response ) {
+							   // Fetch data
+							   $.ajax({
+							    url: "../clases/Search.php",
+							    type: 'post',
+							    dataType: "json",
+							    data: {
+							     search: request.term
+							    },
+							    success: function( data ) {
+							     response( data );
+							    }
+							   });
+							  },
+							  select: function (event, ui) {
+							   // Set selection
+							   $('#key').val(ui.item.label); // display the selected text
+							   $('#selectuser_id').val(ui.item.value); // save selected id to input
+							   return false;
+							  }
+						});
+				    });
 	}); 
+
+//--------
+		// $.ajax({
+	 //            type: "POST",
+	 //            url: "../clases/Search.php",
+	 //            data: {key: dataString},
+	 //            success: function(data) {
+	 //            	if (data) {
+	 //            		 // //Escribimos las sugerencias que nos manda la consulta
+		// 	             //    $('#suggestions').fadeIn(1000).html(data);
+		// 	             //    //Al hacer click en alguna de las sugerencias
+		// 	             //    $('.suggest-element').on('click', function(){
+		// 	             //            //Obtenemos la id unica de la sugerencia pulsada
+		// 	             //            var id = $(this).attr('id');
+		// 	             //            //Editamos el valor del input con data de la sugerencia pulsada
+		// 	             //            $('#key').val($('#'+id).attr('data'));
+		// 	             //            //Hacemos desaparecer el resto de sugerencias
+		// 	             //            $('#suggestions').fadeOut(1000);
+		// 	             //            alert('Has seleccionado el '+id+' '+$('#'+id).attr('data'));
+		// 	             //            return false;
+		// 	             //    });
+
+		
+
+	 //            	}
+	 //            	else{
+	 //            		//$("#key").val("");
+	 //            	}
+	               
+	 //            }
+	 //        });
+
+
+
+	//     });
+	// }); 
     </script>
 </body>
 </html>
